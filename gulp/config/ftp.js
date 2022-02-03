31f71b6da05295081a03e0c83a561f685b1496fe
DIGIKRASH <krashmate@gmail.com>
2022-02-03 23:20:58 +0600

    gulpfile в один файл

--- gulp/config/ftp.js
+++
@@ -1,6 +0,0 @@
-export let configFTP = {
-    host: "test--digitalkrash.ru", // Адрес FTP сервера
-    user: "testdigi", // Имя пользователя
-    password: "KraSh359624157864", // Пароль
-    parallel: 5, // Кол-во обновленных потоков
-}
